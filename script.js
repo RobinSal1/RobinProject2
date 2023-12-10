b415eea786c86a69e905e24b5d380b1db5d31bee
Robin <robinsalminen1@gmail.com>
2023-12-11 00:18:23 +0200

    fetch

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function showRates() { // showrates function
     let amount = document.getElementById('amountInput').value; // Get user inputed value
 
     // Create XMLHttpRequest and handle the response
-    let xhr = new XMLHttpRequest(); //activate the HHTP request
-    xhr.open('GET', 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_oq1A4Y9nMCPWlD7qVCsPDVIwpbrmsb9b57M5Qdvp&currencies=USD%2CCAD%2CSEK%2CNZD%2CAUD&base_currency=EUR', true); // HTTP Request method is 'GET'. The URL endpoint specifies the API api parameters like the key,currencies and currency
+    let fetch = new XMLHttpRequest(); //activate the HHTP request
+    fetch.open('GET', 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_oq1A4Y9nMCPWlD7qVCsPDVIwpbrmsb9b57M5Qdvp&currencies=USD%2CCAD%2CSEK%2CNZD%2CAUD&base_currency=EUR', true); // HTTP Request method is 'GET'. The URL endpoint specifies the API api parameters like the key,currencies and currency
     
-    xhr.onload = function() { //Defines the next step when the XMLHttpRequesT load response is activated
+    fetch.onload = function() { //Defines the next step when the XMLHttpRequesT load response is activated
 
-        if (xhr.status == 200) { // checks if the response is successful
+        if (fetch.status == 200) { // checks if the response is successful
             let ratesData = JSON.parse(this.response); //parses the response into an object
             let rates = ratesData.data; //gets the currency rates from the parsed responce
             let feed = document.getElementById('feed'); // retrieve the HTML element with the id 'feed'
@@ -24,7 +24,7 @@ function showRates() { // showrates function
             feed.appendChild(ratesList); // insert the entire ratesList in to the feed element
         }
     };
-    xhr.send(); // Sends the XMLHttpRequest to the currency conversion API
+    fetch.send(); // Sends the XMLHttpRequest to the currency conversion API
 }
 
 const convertButton = document.getElementById('convertButton'); //Gets the Html element with the convertButton and stores in in the convertButton variable
