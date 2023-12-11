c63d986a96ab742f6e4615958538caa28aeecdca
Robin <robinsalminen1@gmail.com>
2023-12-11 10:38:56 +0200

    new

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function showRates() { // showrates function
     let amount = document.getElementById('amountInput').value; // Get user inputed value
 
     // Create XMLHttpRequest and handle the response
-    let fetch = new XMLHttpRequest(); //activate the HHTP request
+    let fetch = new XMLHttpRequest(); //activate the request
     fetch.open('GET', 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_oq1A4Y9nMCPWlD7qVCsPDVIwpbrmsb9b57M5Qdvp&currencies=USD%2CCAD%2CSEK%2CNZD%2CAUD&base_currency=EUR', true); // HTTP Request method is 'GET'. The URL endpoint specifies the API api parameters like the key,currencies and currency
     
     fetch.onload = function() { //Defines the next step when the XMLHttpRequesT load response is activated
